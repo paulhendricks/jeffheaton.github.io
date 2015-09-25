41dc9181873082132c826acd449e81b0d557762c
Jeff Heaton <jeff@jeffheaton.com>
2015-09-25 11:04:14 -0500

    Switching to initial heaton research site version

---
+++ aifh/vol3/deep_belief.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  window.prev_delta = [0,0,0,0,0,0,0,0,0];
+  $("#calculateButton").click(function(){
+	calculate_display();
+
+  	});
+  	
+  	$("#calcAllButton").click(function(){
+  	});
+  	
+	$("#randomizeButton").click(function(){
+	});
+});
---
+++ book/programming-neural-networks-encog3-cs_err.md
@@ -0,0 +1,14 @@
+
+---
+layout: book
+title: "Programming Neural Networks with Encog3 in C#, 2nd Edition"
+isbn: 9781604390261
+author: Jeff Heaton
+pages: 232
+status: Available
+---
+
+Errata
+======
+
+Nothing yet.
